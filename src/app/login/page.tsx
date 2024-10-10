33bad112bf814612003c2879de8b765859572a88
Der_Brasilianer <enzosoddano12-3@gmail.com>
2024-10-09 21:41:06 -0300

    Conversão para a tela básica de notícias

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,25 @@
-import Link from "next/link"
-import LoginForm from "./_components/login-form/login-form"
+import Link from "next/link";
 
-const LoginPage = () => {
-  return <div className='p-3 bg-white rounded shadow mx-auto w-full' style={{ maxWidth: '300px' }}>
-    <h2 className="text-center mb-2">
-      Login
-    </h2>
-    <LoginForm />
-    <footer className="text-center mt-4">
-      Não tem uma conta?<br />
-      <Link className="text-green-500 underline" href="/login/register">Cadastre-se</Link>
-    </footer>
-  </div>
+const NewsPage = () => {
+  return (
+    <div className='p-3 bg-white rounded shadow mx-auto w-full' style={{ maxWidth: '800px' }}>
+      <h1 className="text-center mb-4">Portal de Notícias</h1>
+      <ul className="list-disc pl-5">
+        <li className="mb-2">
+          <Link className="text-blue-500 hover:underline" href="/news/article-1">Notícia 1</Link>
+        </li>
+        <li className="mb-2">
+          <Link className="text-blue-500 hover:underline" href="/news/article-2">Notícia 2</Link>
+        </li>
+        <li className="mb-2">
+          <Link className="text-blue-500 hover:underline" href="/news/article-3">Notícia 3</Link>
+        </li>
+      </ul>
+      <footer className="text-center mt-4">
+        <Link className="text-green-500 underline" href="/news/create">Adicionar Notícia</Link>
+      </footer>
+    </div>
+  );
 }
 
-export default LoginPage
+export default NewsPage;
