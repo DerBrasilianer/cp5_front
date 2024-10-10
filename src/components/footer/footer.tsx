bb6370db1e1d3e55847c475ae79c7dceee510434
Der_Brasilianer <enzosoddano12-3@gmail.com>
2024-10-09 21:44:23 -0300

    Retirar elementos antigos do Footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,29 +5,6 @@ const Footer = () => {
       <p>Enzo Soddano RM557937</p>
       <p>Enzo Dias RM558438</p>
       <p>Vinicius Prates RM559183</p>
-      <div className="mt-4">
-        Desenvolvido por
-        <a
-          href="https://github.com/vinimarcili"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-green-500 hover:underline ml-1"
-        >
-          Vinícius Marcili
-        </a>
-        {' '}@ {new Date().getFullYear()}
-      </div>
-      <div>
-        E modificado por{' '}
-        <a
-          href="https://github.com/DerBrasilianer"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500 hover:underline"
-        >
-          Enzo Soddano
-        </a>
-      </div>
     </footer>
   )
 }
